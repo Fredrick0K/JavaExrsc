38d69c96a1419ad2f0b66fb4ccec971c88cef05c
Frederick0K <moretoxo@gmail.com>
2025-09-21 12:31:51 +0200

    Cambio LeerEmplaedo(Ahora lee todos)

--- Actividades_U1_BinarioSequen/src/acceso/AccesoEmpleado.java
+++ Actividades_U1_BinarioSequen/src/acceso/AccesoEmpleado.java
@@ -21,18 +21,18 @@ public class AccesoEmpleado {
 		ObjectOutputStream flujoSalida1 = null;
 		MyObjectOutputStream flujoSalida2 = null;
 		try {
-			File fichero = new File("data/empleados.dat");
+			File fichero = new File("Actividades_U1_BinarioSequen\\data\\empleados.dat");
 
 			if (fichero.exists()) {
 				flujoSalida2 = new MyObjectOutputStream(new FileOutputStream(fichero, true));
 				flujoSalida2.writeObject(empleado);
 			}
-			// Crear el fichero e insertar la película al principio del fichero.
+			// Crear el fichero e insertar la pelï¿½cula al principio del fichero.
 			else {
 				flujoSalida1 = new ObjectOutputStream(new FileOutputStream(fichero));
 				flujoSalida1.writeObject(empleado);
 			}
-			
+
 		} finally {
 			try {
 				if (flujoSalida1 != null) {
@@ -54,27 +54,24 @@ public class AccesoEmpleado {
 	public static ArrayList<String> leerFichero()
 			throws FileNotFoundException, StreamCorruptedException, IOException, EOFException, ClassNotFoundException {
 		ObjectInputStream flujoEntrada = null;
-		File fichero = new File("data/empleados.dat");
+		File fichero = new File("Actividades_U1_BinarioSequen\\data\\empleados.dat");
 		ArrayList<String> listaEmpleados = new ArrayList<String>();
 		try {
 			flujoEntrada = new ObjectInputStream(new FileInputStream(fichero));
-			int bytesContados = 0;
 			try {
 				while (true) {
 					Empleado empleado = (Empleado) flujoEntrada.readObject();
-					listaEmpleados.add(empleado.toString()); // Añadir empleados al array
-					bytesContados++;
-					return listaEmpleados;
+					listaEmpleados.add(empleado.toString());
 				}
-
-			} finally {
-
+			} catch (EOFException eof) {
+				// Fin del archivo, salir del bucle
 			}
 		} finally {
 			if (flujoEntrada != null) {
 				flujoEntrada.close();
 			}
 		}
-
+		return listaEmpleados;
 	}
+
 }
