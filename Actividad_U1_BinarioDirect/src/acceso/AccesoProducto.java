8c98b588429c4610cf3984c85d5795d0e25db212
Mohammad <Fredrick0K>
2025-09-30 22:51:22 +0200

    Frst commit

---
+++ Actividad_U1_BinarioDirect/src/acceso/AccesoProducto.java
@@ -0,0 +1,6 @@
+package acceso;
+
+public class AccesoProducto {
+//Insertar, Actualizar con codigo, leer TODO, Leer con codigo, eliminar
+	
+}
---
+++ Actividad_U1_BinarioDirect/src/modelo/Producto.java
@@ -0,0 +1,18 @@
+package modelo;
+
+public class Producto {
+	
+	//Constantes
+	public static final int LONGITUD_NOMBRE = 15;
+	public static final int LONGITUD_CATEGORIA = 20;
+	
+	
+	//atributos
+	private int codigo;
+	private String nombre;
+	private String categoria;
+	private Fecha fechaModificacion;
+	private int Cantidad;
+	private double precio;
+	
+}
