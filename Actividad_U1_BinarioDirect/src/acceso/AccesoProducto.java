8865ed02bf9187e895298389ebe4d1fb54217edc
Frederick0K <moretoxo@gmail.com>
2025-10-07 12:28:25 +0200

    Funciona Leer y ecribir

--- Actividad_U1_BinarioDirect/src/acceso/AccesoProducto.java
+++ Actividad_U1_BinarioDirect/src/acceso/AccesoProducto.java
@@ -1,5 +1,6 @@
 package acceso;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 
@@ -31,6 +32,38 @@ public class AccesoProducto {
 		Producto producto = new Producto(codigo, nombre, categoria, fechaModificacion, cantidad, precio);
 		return producto;
 		
+	}
+
+	public static boolean escribirProducto(RandomAccessFile flujoSalida, Producto producto) throws IOException {
+
+		if (producto.getCodigo() <= 0) {
+			return false;
+		}
+
+		File file = new File("Actividad_U1_BinarioDirect\\data\\producto.dat");
+		flujoSalida = new RandomAccessFile(file, "rw");
+		int tamanyoFichero = (int) flujoSalida.length();
+
+		flujoSalida.seek(tamanyoFichero);
+		flujoSalida.writeInt(producto.getCodigo());
+
+		String nombre = producto.getNombre();
+		for (int i = 0; i < Producto.LONGITUD_NOMBRE; i++) {
+			char c = i < nombre.length() ? nombre.charAt(i) : ' ';
+			flujoSalida.writeChar(c);
+		}
+
+		String categoria = producto.getCategoria();
+		for (int i = 0; i < Producto.LONGITUD_CATEGORIA; i++) {
+			char c = i < categoria.length() ? categoria.charAt(i) : ' ';
+			flujoSalida.writeChar(c);
+		}
+
+		flujoSalida.writeBytes(producto.getFechaModificacion().toString() + "\n");
+		flujoSalida.writeInt(producto.getCantidad());
+		flujoSalida.writeDouble(producto.getPrecio());
+
+		return true;
 	}
 
    
--- Actividad_U1_BinarioDirect/src/principal/Principal.java
+++ Actividad_U1_BinarioDirect/src/principal/Principal.java
@@ -1,6 +1,15 @@
 package principal;
 
 import acceso.AccesoProducto;
+import entrada.Teclado;
+import modelo.Fecha;
+import modelo.Producto;
+
+import java.io.File;
+import java.io.IOError;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.RandomAccess;
 import java.util.Scanner;
 
 public class Principal {
@@ -20,22 +29,74 @@ public class Principal {
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
+        RandomAccessFile flujoEntrada = null;
 
-        Scanner teclado = new Scanner(System.in);
-        System.out.println("Opcion: ");
-        int opcion = teclado.nextInt();
+        int opcion;
+        do {
+            escribirMenu();
+            opcion = Teclado.leerEntero("Introduzca una opcion: ");
+            switch (opcion) {
+                case 0:
 
-        switch (opcion) {
-            case 0:
+                    break;
+                case 1:
+                    try {
+                        File fichero = new File("Actividad_U1_BinarioDirect\\data\\producto.dat");
+                        flujoEntrada = new RandomAccessFile(fichero, "r");
+                        flujoEntrada.seek(0);
+                        int contador = 0;
+                        while (flujoEntrada.getFilePointer() < flujoEntrada.length()) {
+                            Producto producto = AccesoProducto.leerProducto(flujoEntrada);
+                            if (producto.getCodigo() > 0) {
+                                System.out.println(producto.toString());
+                                contador++;
+                            }
+                        }
+                    } catch (IOException ioe) {
+                        System.out.println("Error al leer del fichero binario.");
+                        ioe.printStackTrace();
+                    } finally {
+                        try {
+                            if (flujoEntrada != null) {
+                                flujoEntrada.close();
+                            }
+                        } catch (IOException ioe) {
+                            System.out.println("Error al cerrar el fichero binario.");
+                            ioe.printStackTrace();
+                        }
+                    }
+                    break;
+                case 2:
+                    int codigo = Teclado.leerEntero("Introduzca el codigo del producto: ");
+                    String nombre = Teclado.leerCadena("Introduzca el nombre del producto: ");
+                    String categoria = Teclado.leerCadena("Introduzca la categoria del producto: ");
+                    String fechaStr = Teclado.leerCadena("Introduzca la fecha de modificacion (dd/mm/aaaa): ");
+                    Fecha fechaModificacion = new Fecha(fechaStr);
+                    int cantidad = Teclado.leerEntero("Introduzca la cantidad del producto: ");
+                    double precio = Teclado.leerReal("Introduzca el precio del producto: ");
+                    Producto producto = new Producto(codigo, nombre, categoria, fechaModificacion, cantidad, precio);
+                    try {
+                        File fichero = new File("Actividad_U1_BinarioDirect\\data\\producto.dat");
+                        flujoEntrada = new RandomAccessFile(fichero, "rw");
+                        AccesoProducto.escribirProducto(flujoEntrada, producto);
+                        System.out.println("Se ha escrito un producto en el fichero binario.");
+                    } catch (IOException ioe) {
+                        System.out.println("Error al escribir en el fichero binario.");
+                        ioe.printStackTrace();
+                    } finally {
+                        try {
+                            if (flujoEntrada != null) {
+                                flujoEntrada.close();
+                            }
+                        } catch (IOException ioe) {
+                            System.out.println("Error al cerrar el fichero binario.");
+                            ioe.printStackTrace();
+                        }
+                    }
+                    break;
+            }
 
-                break;
-            case 1:
-			AccesoProducto.leerProducto(flujoEntrada)
-                break;
-            default:
-                throw new AssertionError();
-        }
-
+        } while (opcion != 0);
     }
 
 }
